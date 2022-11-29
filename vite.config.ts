d07bd1c946924c8901fb026681469f53ff58bb79
Christofer Castillo <ccastillo993a@gmail.com>
2022-11-29 18:05:36 -0500

    test: configuración inicial para pruebas vitest y testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    coverage: {
+      provider: "istanbul", // or 'c8'
+      reporter: ["text", "json", "html"],
+    },
+    environment: "happy-dom", // or 'jsdom'
+    setupFiles: "./src/tests/setup.ts",
+  },
 });
