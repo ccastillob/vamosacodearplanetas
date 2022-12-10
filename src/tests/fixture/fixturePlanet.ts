c7e33209ab89ae90f2b61867f43c48211ec633c1
Christofer Castillo <ccastillo993a@gmail.com>
2022-12-10 00:35:23 -0500

    test: agrego un fixture con la data de un planeta

---
+++ src/tests/fixture/fixturePlanet.ts
@@ -0,0 +1,49 @@
+export const fixturePlanet = {
+  id: "63767c2722e583bc1120a312",
+  name: "Earth",
+  rotation: "0.99 days",
+  revolution: "365.26 days",
+  radius: "6,371 km",
+  temperature: "16°c",
+  geology: {
+    id: "63767c2822e583bc1120a315",
+    content:
+      "The total surface area of Earth is about 510 million km2. The continental crust consists of lower density material such as the igneous rocks granite and andesite. Less common is basalt, a denser volcanic rock that is the primary constituent of the ocean floors.",
+    source: "https://en.wikipedia.org/wiki/Earth#Surface",
+    planetId: "63767c2722e583bc1120a312",
+  },
+  images: {
+    planet: {
+      id: "63767c2822e583bc1120a317",
+      small: "/assets/planet-earth-small.svg",
+      medium: "/assets/planet-earth-medium.svg",
+      large: "/assets/planet-earth.svg",
+    },
+    geology: {
+      id: "63767c2822e583bc1120a319",
+      small: "/assets/geology-earth-small.png",
+      medium: "/assets/geology-earth-medium.png",
+      large: "/assets/geology-earth.png",
+    },
+    internal: {
+      id: "63767c2822e583bc1120a318",
+      small: "/assets/planet-earth-internal-small.svg",
+      medium: "/assets/planet-earth-internal-medium.svg",
+      large: "/assets/planet-earth-internal.svg",
+    },
+  },
+  overview: {
+    id: "63767c2722e583bc1120a313",
+    content:
+      "Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth's surface is land with remaining 70.8% is covered with water. Earth's distance from the Sun, physical properties and geological history have allowed life to evolve and thrive.",
+    source: "https://en.wikipedia.org/wiki/Earth",
+    planetId: "63767c2722e583bc1120a312",
+  },
+  structure: {
+    id: "63767c2822e583bc1120a314",
+    content:
+      "Earth's interior, like that of the other terrestrial planets, is divided into layers by their chemical or physical (rheological) properties. The outer layer is a chemically distinct silicate solid crust, which is underlain by a highly viscous solid mantle.",
+    source: "https://en.wikipedia.org/wiki/Earth#Internal_structure",
+    planetId: "63767c2722e583bc1120a312",
+  },
+};
