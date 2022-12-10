753b13a472839a4b8c86c645af873aafc25d76ac
Christofer Castillo <ccastillo993a@gmail.com>
2022-12-10 00:33:13 -0500

    test: componente InfoContent

---
+++ src/tests/components/InfoContent.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import { InfoContent } from "../../components/InfoContent";
+
+describe("Pruebas en el componente <InfoContent />", () => {
+  test("debe de mostrar el nombre y la info del planeta correctamente", () => {
+    const dataInfo = {
+      namePlanet: "Earth",
+      dataCharacteristic: {
+        content: "Esto es una descripción del planeta",
+        source: "https://wikipedia.com/",
+      },
+    };
+    const { namePlanet, dataCharacteristic } = dataInfo;
+
+    render(
+      <InfoContent
+        namePlanet={namePlanet}
+        dataCharacteristic={dataCharacteristic}
+      />
+    );
+
+    const elementHeading: HTMLHeadingElement = screen.getByRole("heading");
+    const elementLink: HTMLAnchorElement = screen.getByRole("link");
+
+    expect(elementHeading.textContent).toContain(namePlanet);
+    expect(screen.getByText(dataCharacteristic.content)).toBeTruthy();
+    expect(elementLink.href).toEqual(dataCharacteristic.source);
+  });
+});
