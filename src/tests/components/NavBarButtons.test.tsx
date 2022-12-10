94d167e8803b07ce24a9c386de793cd029f19bcc
Christofer Castillo <ccastillo993a@gmail.com>
2022-12-10 01:23:50 -0500

    test: componente NavBarButtons

---
+++ src/tests/components/NavBarButtons.test.tsx
@@ -0,0 +1,28 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { vi } from "vitest";
+import { NavBarButtons } from "../../components";
+
+describe("Pruebas en el componente <NavBarButtons />", () => {
+  test("debe de llamarse el hiddenMenu al hacer click en el elemento", () => {
+    const hiddenMenu = vi.fn();
+    const namePlanet = "earth";
+
+    render(<NavBarButtons className="flex" hiddenMenu={hiddenMenu} />);
+
+    const itemSelected = screen.getByText(namePlanet);
+    fireEvent.click(itemSelected);
+
+    expect(hiddenMenu).toHaveBeenCalledTimes(1);
+  });
+
+  test("debe mostrase la clase que le pasamos por props", () => {
+    const hiddenMenu = vi.fn();
+    const navClassName = "flex flex-auto";
+
+    render(<NavBarButtons className={navClassName} hiddenMenu={hiddenMenu} />);
+
+    const navSelected: HTMLElement = screen.getByRole("navigation");
+
+    expect(navSelected.className).toBe(navClassName);
+  });
+});
